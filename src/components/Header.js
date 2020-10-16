2f5f6d2ce6fc6dd09150de68f7833cfb603ea8fd
migzerino <maplaplana13@gmail.com>
2020-10-16 12:00:03 +0800

    added cursor pointer to employee rows

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,30 @@
-import { AppBar, Box, Button, Grid, IconButton, makeStyles, Tab, Tabs, Toolbar, Typography } from '@material-ui/core'
-import React, { useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import MenuIcon from '@material-ui/icons/Menu';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import "fontsource-roboto"
-import PropTypes from 'prop-types';
-import Dashboard from '../pages/Dashboard';
-import Employees from '../pages/Employees';
-
+import {
+    AppBar,
+    Box,
+    Button,
+    Grid,
+    IconButton,
+    makeStyles,
+    Tab,
+    Tabs,
+    Toolbar,
+    Typography,
+} from "@material-ui/core";
+import React, { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import MenuIcon from "@material-ui/icons/Menu";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import "fontsource-roboto";
+import PropTypes from "prop-types";
+import Dashboard from "../pages/Dashboard";
+import Employees from "../pages/Employees";
 
 function a11yProps(index) {
     return {
         id: `nav-tab-${index}`,
-        'aria-controls': `nav-tabpanel-${index}`,
+        "aria-controls": `nav-tabpanel-${index}`,
     };
 }
-
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -27,11 +36,7 @@ function TabPanel(props) {
             aria-labelledby={`nav-tab-${index}`}
             {...other}
         >
-            {value === index && (
-                <Box>
-                    {children}
-                </Box>
-            )}
+            {value === index && <Box>{children}</Box>}
         </div>
     );
 }
@@ -41,22 +46,19 @@ const useStyles = makeStyles((theme) => ({
         borderBottom: `1px solid ${theme.palette.divider}`,
     },
     toolbar: {
-        flexWrap: 'wrap',
+        flexWrap: "wrap",
     },
     brand: {
         // flexGrow: 1,
         margin: theme.spacing(1, 1.5),
         padding: theme.spacing(1, 4),
     },
-    navbar__contents: {
-
-    },
+    navbar__contents: {},
     alignedTabs: {
         position: "relative",
-        top: "8px"
+        top: "8px",
     },
 }));
-
 
 function Header(props) {
     const classes = useStyles();
@@ -66,24 +68,34 @@ function Header(props) {
     const pages = ["home", "employees", "attendance", "payslip"];
     let currentPage = pages.indexOf(pageName);
 
-    const [currentPageNum, setPageNum] = useState(typeof (pageName) === "undefined" ? 0 : currentPage);
+    const [currentPageNum, setPageNum] = useState(
+        typeof pageName === "undefined" ? 0 : currentPage
+    );
     // console.log(typeof (pageName));
 
     const pageChange = (e, newPage) => {
-        history.push(`${pages[newPage]}`)  //pushes to a new page
-        setPageNum(newPage);   //sets new page for tab
-    }
+        history.push(`${pages[newPage]}`); //pushes to a new page
+        setPageNum(newPage); //sets new page for tab
+    };
 
     return (
         <div className={classes.root}>
-            <AppBar position="sticky" color="primary" className={classes.appBar}>
+            <AppBar
+                position="sticky"
+                color="primary"
+                className={classes.appBar}
+            >
                 {/* <Toolbar className={classes.toolbar}> */}
 
                 <Grid container className={classes.navbar__contents}>
-
-                    <Typography variant="h6" color="inherit" noWrap className={classes.brand}>
+                    <Typography
+                        variant="h6"
+                        color="inherit"
+                        noWrap
+                        className={classes.brand}
+                    >
                         POWERGEAR INC.
-                     </Typography>
+                    </Typography>
                     <Tabs
                         variant="fullWidth"
                         value={currentPageNum}
@@ -91,7 +103,7 @@ function Header(props) {
                         aria-label="nav tabs example"
                         classes={{ flexContainer: classes.alignedTabs }}
                     >
-                        <Tab label="Home"  {...a11yProps(0)} />
+                        <Tab label="Home" {...a11yProps(0)} />
                         <Tab label="Employees" {...a11yProps(1)} />
                         <Tab label="Attendance" {...a11yProps(2)} />
                         <Tab label="Payslip" {...a11yProps(3)} />
@@ -110,7 +122,7 @@ function Header(props) {
             </TabPanel>
             {/* </Toolbar> */}
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
